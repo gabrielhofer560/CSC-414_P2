1f8972e23538e73bd7cf2689cc6956cce61d8fe1
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-03-30 21:44:18 -0500

    _

--- dscr.py
+++ dscr.py
@@ -22,41 +22,56 @@ using image pyramids), or come up with your own ideas.
 """
 returns histogram?
 """
+
+def printH(H):
+    for i in H: print(str(i)+' ',end='')
+
+
 def makeH(window):
     H=np.zeros(8)
     for i in range(1,5):
         for j in range(1,5):
             m = math.sqrt((W[i+1,j]-W[i-1,j])**2 + (W[i,j+1]-W[i,j-1])**2)
             theta = atan((W[i,j+1]-W[i,j-1]) / (W[i+1,j]-W[i-1,j]))
-            # put stuff in bins
-            norm = (theta+math.pi/2)*8/math.pi
-            H[norm] += m
-    # find max bin 
+            norm=(theta+math.pi/2)*8/math.pi
+            H[norm]+=m
     m=0
     theta=-1
-    for i in range(8):
-        if H[i] > mx: 
-            m=b
+    for i,j in enumerate(H):
+        if j>mx:
+            m=j
             theta=i
+
+    printH(H)
+
     return [m,theta]
 
 """
 Sift Feature Desciptor
 makes HOG: Histogram of Oriented Gradients
 :param feat - list of interest points a.k.a. keypoints
-:returns something...
+:returns 
 """
 def sift(img,feat):
+    dscr=[]
     for i in feat:
         r,c=i[0],i[1]
-        A=np.zeros((4,4))
-        L=gk(6,6,1)*img[r-9+(j*4):r-3+(j*4),c-9+(k*4):c-3+(k*4)]
+        A=np.zeros((4,4,2)) 
         for j in range(4):
             for k in range(4):
-                A[j,k]=makeH((L[r-9+(j*4):r-3+(j*4),c-9+(k*4):c-3+(k*4)]))
-    B=np.zeros(8)
-    for i in range(4):
+                A[j,k]=makeH((gk(6,6,1)*img[r-9+(j*4):r-3+(j*4),c-9+(k*4):c-3+(k*4)]))
+        B=np.zeros(8)
         for j in range(4):
+            for k in range(4):
+                B[A[j,k,0]]+=A[j,k,1]
+        m=0
+        theta=-1
+        for i,j in enumerate(B):
+            if j>mx:
+                m=j
+                theta=i
+        dscr.append([m,theta])
+
 
 
 
---
+++ what.py
@@ -0,0 +1,18 @@
+
+
+r,c=8,8
+for j in range(4):
+    for k in range(4):
+        print(r-9+(j*4))
+        print(r-3+(j*4))
+        print(c-9+(k*4))
+        print(c-3+(k*4))
+        print()
+
+
+
+
+
+
+
+ 
