65893249928b14cd6f9021ec97194da1a69e0b94
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-03-30 21:48:43 -0500

    _

--- driver.py
+++ driver.py
@@ -3,10 +3,6 @@ Author: Gabriel Hofer
 Course: CSC-414
 Instructor: Dr. Randy Hoover
 """
-"""
-https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
-https://stackoverflow.com/questions/49643907/clipping-input-data-to-the-valid-range-for-imshow-with-rgb-data-0-1-for-floa
-"""
 from imfilter import imfilter
 from gk import gk
 from harris import harris
@@ -19,11 +15,23 @@ from skimage.color import rgb2gray
 
 img = io.imread(sys.argv[1]);
 cpy = img
-img = rgb2gray(img)
+gry = rgb2gray(img)
 
-feat = harris(img,gk(3,3,1),0.4)
-print(len(feat))
-showFeatures(cpy,feat)
+feat = harris(gry,gk(3,3,1),0.4)
+print("number of features: "+str(len(feat)))
+showFeatures(img,feat)
+
+
+
+import dscr
+
+D = sift(img,feat)
+
+# showFeatures2()
+
+
+
+
 
 
 #io.imshow((img * 255).astype(np.uint8)  , vmin=0, vmax=255, cmap="gray")
--- showFeatures.py
+++ showFeatures.py
@@ -15,7 +15,6 @@ img = cv2.imread('j.png',0)
 def showFeatures(img,feat):
     for i in feat:
         img[i[0],i[1],:]=[255,0,0]
-
     #io.imshow((img * 255).astype(np.uint8)  , vmin=0, vmax=255, cmap="gray")
     io.imshow(img,vmin=0,vmax=255)
     io.show()
