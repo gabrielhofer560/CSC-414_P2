f031bbfba8c4edba7b627c6870346a60996c9125
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-04-02 21:40:26 -0500

    _

--- drawMatches.py
+++ drawMatches.py
@@ -5,22 +5,46 @@ draw lines showing feature matches
 import cv2
 import math
 import numpy as np
-
-def drawLine(img,one,two,c,th):
-    x0,y0=one
-    x1,y1=two
-    if x0==x1: return
-    dx=x1-x0
-    dy=y1-y0
-    print(str(one)+" "+str(two))
-    for x in range(x0,x1+1):
-        y = y0+((dy*(x-x0))/dx)
-        img[int(x),int(y)]=c
-
-
 from showDots import showDots 
 from skimage import io
 
+
+"""
+Description - draws a blue line
+:param img - image
+:param one - (x0,y0)
+:param two - (x1,y1)
+:param c - color
+"""
+def line1(img,one,two,c):
+    x0,y0=one
+    x1,y1=two
+    print("hello 1")
+    if y0==y1: return
+    print("hello 2")
+    dx=x1-x0
+    dy=y1-y0
+    step=max(abs(dx),abs(dy))
+    dx/=step
+    dy/=step
+    x=x0
+    y=y0
+    i=0
+    while i<step:
+        img[int(x),int(y)]=c
+        x+=dx
+        y+=dy
+        i+=1
+
+
+"""
+Description - draws lines between matching descriptors
+:param img0 - image 1
+:param kp0 - keypoints 1
+:param img1 - image 2
+:param kp1 - keypoints 2
+:param matches - matches keypoints 
+"""
 def drawMatches(img0,kp0,img1,kp1,matches):
 
     img2 = np.zeros((
@@ -31,34 +55,14 @@ def drawMatches(img0,kp0,img1,kp1,matches):
     img2[0:img0.shape[0],0:img0.shape[1]] = img0
     img2[0:img1.shape[0],img0.shape[1]:img0.shape[1]+img1.shape[1]] = img1
 
-
-#    showDots(img2,kp0)
-#    io.imshow(img2)
-#    io.show()
-
-    cpy=list(kp1)
-    nkp1=[]
     # translate coordinate of right image
+    nkp1=[]
     for i in range(len(kp1)):
         nkp1.append([kp1[i][0],img0.shape[1]+kp1[i][1]])
-        #kp1[i][1]=img0.shape[1]+kp1[i][1]
-
-#    showDots(img2,kp1)
-#    io.imshow(img2)
-#    io.show()
-
-    for i in range(len(kp1)):
-        one,two = kp1[i], nkp1[i]
-        print(one)
-        print(two)
-        drawLine(img2,one,two,[0,0,255],2)
-
-    return img2
-
 
     for [i,j] in matches:
         one,two = kp1[i], nkp1[j]
-        drawLine(img2,one,two,[0,0,255],2)
+        line1(img2,one,two,[0,0,255])
 
     return img2
 
